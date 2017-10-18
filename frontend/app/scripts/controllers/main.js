8ff54d2b0e9a00ccf818888c140835bf75f5fe8a
jrzhao <rho.ajax@gmail.com>
2017-10-18 03:55:02 +0100

    first commit

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name owlDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the owlDemoApp
+ */
+angular.module('owlDemoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
