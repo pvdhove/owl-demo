e6673d2be3ecec7f1471d1d2013807f0d72610b6
jrzhao <rho.ajax@gmail.com>
2017-10-21 20:28:55 +0100

    wrap long yaxis label text

--- frontend/app/scripts/directives/main.js
+++ frontend/app/scripts/directives/main.js
@@ -76,11 +76,12 @@ angular.module('owlDemoApp')
         var w = rawSvg.attr("width");
         var h = rawSvg.attr("height");
         var margin = {top: 20, right: 30, bottom: 40, left: 30};
-        var width = w - margin.left - margin.right;
+        var width  = w - margin.left - margin.right;
         var height = h - margin.top - margin.bottom;
+        var color  = d3.scale.category20();
 
         var xScale = d3.scale.linear()
-          .range([padding + 5, width - padding])
+          .range([padding + (width / 3), width - padding])
           .domain([0, 1.0]);
         var xAxisGen = d3.svg.axis().scale(xScale).orient("bottom");
         //xScale.domain(d3.extent(salesDataToPlot, function(d) { return d.value; })).nice();
@@ -89,6 +90,31 @@ angular.module('owlDemoApp')
           .attr("class", "x axis")
           .attr("transform", "translate(0," + height + ")")
           .call(xAxisGen);
+
+
+          function wrap(text, width) {
+            text.each(function() {
+              var text = d3.select(this),
+                  words = text.text().split(/\s+/).reverse(),
+                  word,
+                  line = [],
+                  lineNumber = 0,
+                  lineHeight = 1.1, // ems
+                  y = text.attr("y"),
+                  dy = parseFloat(text.attr("dy")),
+                  tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+              while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if (tspan.node().getComputedTextLength() > width) {
+                  line.pop();
+                  tspan.text(line.join(" "));
+                  line = [word];
+                  tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                }
+              }
+            });
+          }
 
         function drawLineChart() {
           salesDataToPlot.sort((a, b) => b.prop - a.prop);
@@ -106,7 +132,10 @@ angular.module('owlDemoApp')
           svg.append("g")
             .attr("class", "y axis")
             .attr("transform", "translate(" + xScale(0) + ",0)")
-            .call(yAxisGen);
+            .call(yAxisGen)//;
+            .selectAll(".y text")
+            .call(wrap, (width / 3));
+
 
           var bars = svg.selectAll(".bar").data(salesDataToPlot);
 
@@ -118,14 +147,25 @@ angular.module('owlDemoApp')
             .attr("x", function(d) { return xScale(Math.min(0, d.prop)); })
             .attr("y", function(d) { return yScale(d.class); })
             .attr("width", function(d) { return Math.abs(xScale(d.prop) - xScale(0)); })
+            .attr('fill', function(d) { return color(d.prop); })
             .attr("height", yScale.rangeBand());
-          //bars.exit().remove();
+
+          /*
+          bars
+            .enter()
+            .append("text")
+            .attr("fill", "#fff")
+            .attr("y", function(d, i){return i * 30 + 10;})
+            .attr("x", 15)
+            .text(function(d){return d.class;}); */
+
         }
 
         scope.$watchCollection(exp, function(newVal, oldVal){
           //alert("old:", oldVal);
           salesDataToPlot = newVal;
           //console.log('fuck', salesDataToPlot);
+          //svg.selectAll('*').remove();
           drawLineChart();
         });
 
